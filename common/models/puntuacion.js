1675c2007e245abf9d03603eeb43668e15d247e8
Mariano <1784443@alu.murciaeduca.es>
2017-05-08 08:33:01 +0200

    CRD con angular

--- common/models/puntuacion.js
+++ common/models/puntuacion.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = function(Puntuacion) {
-
+module.exports = function(puntuacion) {
+	puntuacion.observe('before save', function (ctx, next) {
+		if (ctx.isNewInstance) {
+			if (ctx.instance) {
+				if (ctx.options && ctx.options.accessToken && ctx.options.accessToken.userId) {
+					ctx.instance.usuarioId = ctx.options.accessToken.userId;
+				}
+			} else {
+				delete ctx.data.verificado;
+			}
+		}
+		next();
+	});
 };
